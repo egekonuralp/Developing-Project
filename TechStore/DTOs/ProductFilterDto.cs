0fc363d3a87a897852291234b72337a24e5aad83
Ege Konuralp <egekonuralp99@gmail.com>
2026-09-06 23:24:11 +0300

    Ürün Azaltma Butonu Editlendi.

--- TechStore/DTOs/ProductFilterDto.cs
+++ TechStore/DTOs/ProductFilterDto.cs
@@ -10,8 +10,25 @@
 
         public decimal? MaxPrice { get; set; }
 
-        public int Page { get; set; } = 1;
+        private int _page = 1;
 
-        public int PageSize { get; set; } = 10;
+        //Minimum 1'e Sabitliyoruz.
+        public int Page
+        {
+            get => _page;
+            set => _page = value < 1 ? 1 : value;
+        }
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value is < 1 or > 100 ? 10 : value;
+        }
+
+        //Page: 0/negatif gelirse otomatik 1'e sabitleniyor.
+        //PageSize: 1'in altı ya da 100'ün üstü gelirse
+        //(aşırı büyük istekleri engellemek için üst sınır 100), varsayılan olan 10'a düşüyor.
     }
 }
